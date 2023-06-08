a89c5836541608584979ac34914b94160154832e
naga115 <huy1152002@gmail.com>
2023-06-08 09:42:41 +0700

    add exc cookies

--- models/dishes.js
+++ models/dishes.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Currency = mongoose.Types.Currency;
 const commentSchema = new Schema(
   {
     rating: { type: Number, min: 1, max: 5, required: true },
@@ -12,6 +13,11 @@ const dishSchema = new Schema(
   {
     name: { type: "string", required: true, unique: true },
     description: { type: "string", required: true },
+    image: { type: "string", required: true },
+    category: { type: "string", required: true },
+    label: { type: "string", defailt: "" },
+    price: { type: "number", required: true, min: 0 },
+    featured: { type: "boolean", required: true, default: false },
     comments: [commentSchema],
   },
   { timestamps: true }
