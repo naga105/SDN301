98a6d01f883c368b493b919d83b8532cd39db0ad
huy <huy1152002@gmail.com>
2023-06-22 09:16:07 +0700

    add

--- app.js
+++ app.js
@@ -1,195 +1,78 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const leaderRouter = require("./routes/leaderRouter");
-const dishRouter = require("./routes/dishRouter");
-const promoRouter = require("./routes/promoRouter");
+var passport = require("passport");
+var authenticate = require("./service/authenticate");
+const tutorialRouter = require("./routes/tutorialRouter");
+const usersRouter = require("./routes/user");
+// const auth = require("./service/auth");
+var session = require("express-session");
+var config = require("./config");
+var FileStore = require("session-file-store")(session);
+
+const url = config.mongoUrl;
 
 const app = express();
-//basic auth
-// function auth(req, res, next) {
-//   console.log(req.headers);
-//   var authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//     var err = new Error("You are not authenticated");
-//     res.setHeader("WWW-Authenticate", "Basic");
-//     err.status = 401;
-//     next(err);
-//     return;
-//   }
-//   var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-//     .toString()
-//     .split(":");
-//   var user = auth[0];
-//   var pass = auth[1];
-//   if (user === "admin" && user === "password") {
-//     next();
-//   } else {
-//     var err = new Error("You are not a");
-//     res.setHeader("WWW-Authenticate", "Basic");
-//     err.status = 401;
-//     next();
-//   }
-// }
-
-// cookie auth
-// app.use(cookieParser("12345-67890"));
-// function auth(req, res, next) {
-//   if (!req.signedCookies.user) {
-//     var authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//       var err = new Error("You are not authenticated!");
-//       res.setHeader("WWW-Authenticate", "Basic");
-//       err.status = 401;
-//       next(err);
-//       return;
-//     }
-//     var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-//       .toString()
-//       .split(":");
-//     var user = auth[0];
-//     var pass = auth[1];
-//     if (user == "admin" && pass == "password") {
-//       res.cookie("user", "admin", { signed: true });
-//       next(); // authorized
-//     } else {
-//       var err = new Error("You are not authenticated!");
-//       res.setHeader("WWW-Authenticate", "Basic");
-//       err.status = 401;
-//       next(err);
-//     }
-//   } else {
-//     if (req.signedCookies.user === "admin") {
-//       next();
-//     } else {
-//       var err = new Error("You are not authenticated!");
-//       err.status = 401;
-//       next(err);
-//     }
-//   }
-// }
-
-//session auth
-// app.use(session({
-//   name: 'session-id',
-//   secret: '12345-67890-09876-54321',
-//   saveUninitialized: false,
-//   resave: false,
-//   store: new FileStore()
-// }));
-
-// function auth (req, res, next) {
-//     console.log(req.session);
-
-//     if (!req.session.user) {
-//         var authHeader = req.headers.authorization;
-//         if (!authHeader) {
-//             var err = new Error('You are not authenticated!');
-//             res.setHeader('WWW-Authenticate', 'Basic');
-//             err.status = 401;
-//             next(err);
-//             return;
-//         }
-//         var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//         var user = auth[0];
-//         var pass = auth[1];
-//         if (user == 'admin' && pass == 'password') {
-//             req.session.user = 'admin';
-//             next(); // authorized
-//         } else {
-//             var err = new Error('You are not authenticated!');
-//             res.setHeader('WWW-Authenticate', 'Basic');
-//             err.status = 401;
-//             next(err);
-//         }
-//     }
-//     else {
-//         if (req.session.user === 'admin') {
-//             console.log('req.session: ',req.session);
-//             next();
-//         }
-//         else {
-//             var err = new Error('You are not authenticated!');
-//             err.status = 401;
-//             next(err);
-//         }
-//     }
-// }
-
-//mongoose auth
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
-    var err = new Error("You are not authenticated!");
-    err.status = 403;
-    return next(err);
-  } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
-  }
-}
-
-// app.use(auth);
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser("12345-67890"));
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/leaders', leaderRouter);
-// app.use('/promotions', promoRouter);
-app.use("/dishes", dishRouter);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const Dishes = require("./models/dishes");
-
-const url = "mongodb://127.0.0.1:27017/conFusion";
+// const DBUrl = "mongodb://127.0.0.1:27017/tutorial";
 const connect = mongoose.connect(url);
 
 connect.then(
   (db) => {
-    console.log("Connected correctly to server");
+    console.log("Connected correctly to db");
   },
   (err) => {
     console.log(err);
   }
 );
 
+// app.use(auth);
+
+// app.use('/', indexRouter);
+app.use("/tutorials", tutorialRouter);
+app.use("/users", usersRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// function auth(req, res, next) {
+//   console.log(req.user);
+
+//   if (!req.user) {
+//     var err = new Error("You are not authenticated!");
+//     err.status = 403;
+//     next(err);
+//   } else {
+//     next();
+//   }
+// }
+
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  console.log(err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  return res.status(err.statusCode || 500).json({ message: err.message });
 });
 
 module.exports = app;
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,265 +1,60 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const Dishes = require("../models/dishes");
-const dishRouter = express.Router();
+var authenticate = require("../service/authenticate");
 
-dishRouter.use(bodyParser.json());
-dishRouter
-  .route("/")
-  .get((req, res, next) => {
-    Dishes.find({})
-      .then(
-        (dishes) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(dishes);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .post((req, res, next) => {
-    Dishes.create(req.body)
-      .then(
-        (dish) => {
-          console.log("Dish create:", dish);
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(dish);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .put((req, res, next) => {
-    res.statusCode = 403;
-    res.end("Put op isnt supported dish");
-  })
-  .delete((req, res, next) => {
-    Dishes.deleteMany({})
-      .then(
-        (resp) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(resp);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  });
+var express = require("express");
+var router = express.Router();
 
-dishRouter
-  .route("/:dishId")
-  .get((req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dishes) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(dishes);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .post((req, res, next) => {
-    res.statusCode = 403;
-    res.end("Post not support");
-  })
-  .put((req, res, next) => {
-    Dishes.findByIdAndUpdate(
-      req.params.dishId,
-      { $set: req.body },
-      { new: true }
-    )
-      .then(
-        (resp) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(resp);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .delete((req, res, next) => {
-    Dishes.findByIdAndRemove(req.params.dishId)
-      .then(
-        (resp) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(resp);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  });
-dishRouter
-  .route("/:dishId/comments")
-  .get((req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dish) => {
-          if (dish != null) {
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application-json");
-            res.json(dish.comments);
-          } else {
-            err = new Error(`Dish ${req.params.dishId} not found`);
-            err.status = 404;
-            return next(err);
-          }
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .post((req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dish) => {
-          if (dish != null) {
-            dish.comments.push(req.body);
-            dish.save().then(
-              (dish) => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application-json");
-                res.json(dish);
-              },
-              (err) => next(err)
-            );
-          } else {
-            err = new Error(`Dish ${req.params.dishId} not found`);
-            err.status = 404;
-            return next(err);
-          }
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .put((req, res, next) => {
-    res.statusCode = 403;
-    res.end(
-      `PUT operation not supported on /dishes/${req.params.dishId}/comments`
-    );
-  })
-  .delete((req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dish) => {
-          if (dish != null) {
-            dish.comments.forEach(({ _id }) => {
-              dish.comments.id(_id).remove();
-            });
+router.all("/", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  next();
+});
 
-            dish.save().then(
-              (dish) => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application-json");
-                res.json(dish);
-              },
-              (err) => next(err)
-            );
-          } else {
-            err = new Error(`Dish ${req.params.dishId} not found`);
-            err.status = 404;
-            return next(err);
-          }
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  });
-dishRouter
-  .route("/:dishId/comments/:commentId")
-  .get((req, res, next) => {
-    Dishes.findById(req.params.dishId).then((dish) => {
-      if (dish != null && dish.comments.id(req.params.commentId) != null) {
-        res.writeHead(200, {
-          "Content-Type": "application/json",
-        });
-        res.json(dish.comments.id(req.params.commentId));
-      } else if (dish == null) {
-        err = new Error(`Dish ${req.params.dishId} not found`);
-        err.status = 404;
-        return next(err);
-      } else {
-        err = new Error(`Dish ${req.params.commentId} not found`);
-        err.status = 404;
-        return next(err);
-      }
-    });
-  })
-  .post((req, res, next) => {
-    res.statusCode = 403;
-    res.end(
-      `POST operation not supported on /dishes/${req.params.dishId}/comments/${req.params.commentId}`
-    );
-  })
-  .put((req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dish) => {
-          if (dish != null && dish.comments.id(req.params.commentId) != null) {
-            if (res.body.rating) {
-              dish.comments.id(req.params.commentId).rating = req.body.rating;
-            }
-            if (req.body.comment) {
-              dish.comments.id(req.params.commentId).comment = req.body.comment;
-            }
-            dish.save().then(
-              (dish) => {
-                res.writeHead(200, {
-                  "Content-Type": "application/json",
-                });
-                res.json(dish);
-              },
-              (err) => next(err)
-            );
-          } else if (dish == null) {
-            err = new Error(`Dish ${req.params.dishId} not found`);
-            err.status = 404;
-            return next(err);
-          } else {
-            err = new Error(`Dish ${req.params.commentId} not found`);
-            err.status = 404;
-            return next(err);
-          }
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .delete((req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dish) => {
-          if (dish != null && dish.comments.id(req.params.commentId) != null) {
-            dish.comments.id(req.params.commentId).remove();
-            dish.save().then(
-              (dish) => {
-                res.writeHead(200, {
-                  "Content-Type": "application/json",
-                });
-                res.json(dish);
-              },
-              (err) => next(err)
-            );
-          } else if (dish == null) {
-            err = new Error(`Dish ${req.params.dishId} not found`);
-            err.status = 404;
-            return next(err);
-          } else {
-            err = new Error(`Dish ${req.params.commentId} not found`);
-            err.status = 404;
-            return next(err);
-          }
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  });
+router.get("/", (req, res, next) => {
+  res.end("Will send all the dishes to you");
+});
 
-module.exports = dishRouter;
+router.post("/", authenticate.verifyUser, (req, res, next) => {
+  res.end(
+    "Will add the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+router.put("/", authenticate.verifyUser, (req, res, next) => {
+  res.statusCode = 403;
+  res.end("PUT operation is not supported on /dished");
+});
+
+router.delete("/", authenticate.verifyUser, (req, res, next) => {
+  res.end("Deleting all dishes");
+});
+
+// http://localhost:8080/dishes/3
+router.get("/:dishId", (req, res, next) => {
+  res.end("Will send details of the dish: " + req.params.dishId + " to you!");
+});
+
+router.post("/:dishId", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("POST operation is not supported on /dishes/" + req.params.dishId);
+});
+
+router.put("/:dishId", (req, res, next) => {
+  res.write("Updating the dish: " + req.params.dishId + "\n");
+  res.end(
+    "Will update the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+router.delete("/:dishId", (req, res, next) => {
+  res.end("Deleting the dish: " + req.params.dishId);
+});
+
+module.exports = router;
+
+module.exports = router;
